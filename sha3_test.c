89b309d9c829dd10cff46712be5ee72300c70d40
Max Danielsson <max@autious.net>
2017-04-17 17:56:52 +0200

    Make sha3_test.c more of an example of usage

--- sha3_test.c
+++ sha3_test.c
@@ -1,3 +1,11 @@
+//Include as header
+#include "sha3.h"
+
+
+//Include as a body implementation
 #define SHA3_BODY_IMPLEMENTATION
 #define SHA3_TEST
 #include "sha3.h"
+
+//Note that including the header implementation before body implementation isn't necessary, 
+//but it demonstrates that it doesn't cause a conflict
